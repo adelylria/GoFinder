e0e0534b9ae90d37bfd206072281123241b356d5
Adrian Gonzalez <adriansa53@gmail.com>
2026-05-16 21:31:05 +0200

    feat(fix): arrow keys to move between the list

--- core/hotkey/hotkey.go
+++ core/hotkey/hotkey.go
@@ -1,6 +1,9 @@
 package hotkey
 
-import "fyne.io/fyne/v2/widget"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
 
 type HotkeyManager struct {
 	ToggleHandler func()
@@ -18,6 +21,23 @@ func NewKeyEventInterceptor() *KeyEventInterceptor {
 	e := &KeyEventInterceptor{}
 	e.ExtendBaseWidget(e)
 	return e
+}
+
+func (e *KeyEventInterceptor) TypedKey(ev *fyne.KeyEvent) {
+	switch ev.Name {
+	case fyne.KeyDown:
+		if e.OnKeyDown != nil {
+			e.OnKeyDown()
+			return
+		}
+	case fyne.KeyUp:
+		if e.OnKeyUp != nil {
+			e.OnKeyUp()
+			return
+		}
+	}
+
+	e.Entry.TypedKey(ev)
 }
 
 func NewHotkeyManager(toggle func(), exit func()) *HotkeyManager {
