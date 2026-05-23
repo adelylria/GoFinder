4670d6d41d46543652cd0b6824ce5422a21548f7
Adrian Gonzalez <adriansa53@gmail.com>
2026-05-23 20:30:23 +0200

    refactor(hotkey): add explanatory stub for Unix ListenHotkeys

--- core/hotkey/hotkey_unix.go
+++ core/hotkey/hotkey_unix.go
@@ -7,7 +7,9 @@ func SetupHotkey(toggle KeyBinding, exit KeyBinding, handler func(int)) {
 	// Hotkey setup is not implemented for Unix-like systems in this version.
 }
 
-func (hm *HotkeyManager) ListenHotkeys() {}
+func (hm *HotkeyManager) ListenHotkeys() {
+	// Hotkey listening is not implemented for Unix-like systems in this version.
+}
 
 func NewUnixHotkeyManager(toggle, exit func()) *HotkeyManager {
 	return &HotkeyManager{
