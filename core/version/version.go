418628863028d239eac8f5122229a30b63db0225
Adrian Gonzalez <adriansa53@gmail.com>
2026-06-07 18:36:33 +0200

    version: add repo metadata and release URLs

---
+++ core/version/version.go
@@ -0,0 +1,15 @@
+package version
+
+const (
+	AppName    = "GoFinder"
+	RepoOwner  = "adelylria"
+	RepoName   = "GoFinder"
+	RepoURL    = "https://github.com/adelylria/GoFinder"
+	ReleaseURL = "https://github.com/adelylria/GoFinder/releases"
+)
+
+var (
+	Version   = "v0.3"
+	Commit    = "unknown"
+	BuildDate = "unknown"
+)
